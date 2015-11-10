69d8f191f9efba74801ad8ba1e4a03072b6d4d7b
kevmo <k.james.moore@gmail.com>
2015-11-09 19:29:53 -0500

    Now watch me WIP

--- app/static/main.js
+++ app/static/main.js
@@ -1,11 +1,28 @@
 //
 var countries;
 
-var CountryInFocus = React.createClass({});
+var CountryInFocus = React.createClass({
+    render: function(){
+        return (<h2>{this.props.name}</h2>)
+    }
+});
 
-var CountriesList = React.createClass({});
+var CountriesList = React.createClass({
+    render: function(){
+        var rows = [];
+        this.props.countries.forEach(function(country){
+            
+        })
+    }
+});
 
-var CountryRow = React.createClass({});
+var CountryRow = React.createClass({
+    render: function(){
+        return (
+            <li>this.props.country.name</li>
+        )
+    }
+});
 
 var CountryBox = React.createClass({});
 
@@ -13,8 +30,17 @@ var CountryBox = React.createClass({});
 var SelectCountryTable = React.createClass({
     getInitialState: function(){
         return {
-            FocusBar:
+            country_in_focus: '',
         }
+    },
+
+    render: function(){
+        return (
+            <div>
+                <CountryInFocus
+                />
+            </div>
+        )
     }
 });
 
